366091120821960ecd9839bd1b6c2d2c523bf6ce
Thiago Ribeiro <thiagoribeiroramos_@outlook.com>
2025-01-31 23:32:23 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,11 +52,12 @@ async function sendMessage(prompt) {
 
     try {
         // Faz a requisição para a API Gemini
-        const response = await fetch(`${URL}?key=${API_KEY}`, {
+        const response = await fetch("/api/proxy", {  
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(jsonRequest)
         });
+
 
         if (!response.ok) throw new Error(`Erro: ${response.status} - ${response.statusText}`);
         
